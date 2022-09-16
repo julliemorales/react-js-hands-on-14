579ec42b5e95353ce22a92da4a16f34c03da4223
Jullie Angeli Morales <110211781+julliemorales@users.noreply.github.com>
2022-09-16 16:17:22 +0800

    Updated Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -27,7 +27,7 @@ const Footer = () => {
       </div>
       <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
       <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
-        © 2022 <span className="hover:underline">Jullibee©</span>. All Rights
+        © 2022 <span className="hover:underline">WD15© Jullie & Jeff</span>. All Rights
         Reserved.
       </span>
     </footer>
